4978ec1c986281e73ba1b357b3fa4452247571ea
Gina Sprint <sprint@gonzaga.edu>
2020-10-02 10:27:30 -0700

    end of error handling

--- MoreSwiftFunS1/main.swift
+++ MoreSwiftFunS1/main.swift
@@ -10,17 +10,75 @@ import Foundation
 
 print("Hello, World!")
 
-func getPositiveInteger() -> Int {
-    print("Enter a positive integer:")
-    if let inputString = readLine() {
-        if let inputInt = Int(inputString) {
-            if inputInt > 0 {
-                return inputInt
-            }
-        }
-    }
-    return -1
+// warm up task
+//func getPositiveInteger() -> Int {
+//    print("Enter a positive integer:")
+//    if let inputString = readLine() {
+//        if let inputInt = Int(inputString) {
+//            if inputInt > 0 {
+//                return inputInt
+//            }
+//        }
+//    }
+//    return -1
+//}
+//
+//let value = getPositiveInteger()
+//print("got:", value)
+
+// MARK: - Guard
+// use guard when you want an early exit from a function
+// when a condition fails
+// useful to prevent heavy if nesting
+// when used with optional binding, the declared let variables
+// are in scope outside the guard
+//func getPositiveInteger() -> Int {
+//    print("Enter a positive integer:")
+//    guard let inputString = readLine(), let inputInt = Int(inputString), inputInt > 0 else {
+//        // here when one of the BCs fails/false
+//        return -1 // early exit because something went wrong
+//    }
+//    // here then all of our BCs succeeded/true
+//    // inputString and inputInt are still in scope here
+//    return inputInt
+//}
+//
+//let value = getPositiveInteger()
+//print("got:", value)
+
+// MARK: - Error Handling
+// when a programmer anticipates that something might go wrong...
+// they "throw" an error and require the
+// calling code acknowledge/handle the error
+
+// define our Error enum
+enum UserInputError: Error {
+    case invalidInteger
+    case invalidPositiveInteger
 }
 
-let value = getPositiveInteger()
-print("got:", value)
+// put throws after the ()
+func getPositiveInteger() throws -> Int {
+    print("Enter a positive integer:")
+    guard let inputString = readLine(), let inputInt = Int(inputString), inputInt > 0 else {
+        // here when one of the BCs fails/false
+        throw UserInputError.invalidPositiveInteger // early exit because something went wrong
+    }
+    // here then all of our BCs succeeded/true
+    // inputString and inputInt are still in scope here
+    return inputInt
+}
+
+// 2 ways to handle the error
+// 1. try? optional
+//let value = try? getPositiveInteger()
+//print("got:", value)
+// 2. try with do {} catch {}
+do {
+    let value = try getPositiveInteger()
+    print("got:", value)
+}
+catch UserInputError.invalidPositiveInteger {
+    print("Unable to get a positive integer from the user")
+    // try to recover...
+}
